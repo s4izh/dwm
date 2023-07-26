11cc88798159b68755e0616b3ea60de8fd648d37
s4izh <sergiosanz234@gmail.com>
2023-07-26 21:59:40 +0200

    fix white color on spaceblue theme

--- themes/spaceblue.h
+++ themes/spaceblue.h
@@ -3,9 +3,9 @@ static const unsigned int gappx     = 10;        /* gaps between windows */
 
 static char normbgcolor[]           = "#000000";
 static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#f1f1f1";
+static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#7d8fa3";
-static char selbordercolor[]        = "#f1f1f1";
+static char selbordercolor[]        = "#bbbbbb";
 static char selbgcolor[]            = "#000000";
 static char *colors[][3] = {
        [SchemeNorm] = { normfgcolor, normbgcolor, normbgcolor },
