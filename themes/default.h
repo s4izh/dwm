555c11fa4b3076f7f621f23763d5a6cdf935f128
s4izh <sergiosanz234@gmail.com>
2023-07-27 00:04:11 +0200

    default theme fix

--- themes/default.h
+++ themes/default.h
@@ -1,4 +1,4 @@
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 
 static const char normbgcolor[]     = "#222222";
@@ -6,7 +6,7 @@ static const char normbordercolor[] = "#444444";
 static const char normfgcolor[]     = "#bbbbbb";
 static const char selfgcolor[]      = "#eeeeee";
 static const char selbgcolor[]      = "#005577";
-static const char selbordercolor[]  = "#550000";
+static const char selbordercolor[]  = "#cccccc";
 static char *colors[][3] = {
     /* 	             fg           bg           border           */
     [SchemeNorm] = { normfgcolor, normbgcolor, normbgcolor },
